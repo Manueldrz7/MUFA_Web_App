3a98409e766274760941895a5b6c38c0c7a7387e
DanyAlva <104276700+DanyAlva@users.noreply.github.com>
2025-10-24 16:00:23 -0600

    Update retosFC.ts

--- src/data/retosFC.ts
+++ src/data/retosFC.ts
@@ -1,24 +1,29 @@
 export const RETOS_FC = {
   facil: [
-    "Marca un gol con tu portero",
     "Haz un gol de tiro libre",
     "Anota con un defensa central",
     "Gana con más del 60% de posesión",
     "Anota un gol en el minuto 90 o más",
     "Haz una asistencia con un jugador suplente",
+    "Anota con un jugador zurdo",
+    "Gana un juego sin offsides",
+    "Gana un juego sin tarjetas",
   ],
   medio: [
     "Gana sin recibir goles",
     "Anota con tres jugadores distintos",
     "Marca un gol de cabeza y otro de volea",
     "Gana un partido con menos del 40% de posesión",
-    "Remonta tras ir perdiendo",
+    "Remonta y gana tras ir perdiendo",
+    "Marca un gol con tu portero",
+    "Anota dos goles de cabeza ~chupas~",
   ],
   dificil: [
     "Gana 4-0 o más",
     "Haz un hat-trick con un jugador suplente",
-    "Anota solo con zurdos",
     "Anota desde fuera del área tres veces",
+    "Anota un gol de chilena",
+    "Anota una manita (5 goles) con un jugador",
     //"Gana con un equipo de 4 estrellas o menos",
   ],
 };
